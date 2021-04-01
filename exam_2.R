1d9d03497348d320714c7cfad675156a7a389205
emeliefoster <emf6966@louisiana.edu>
2021-04-01 14:05:01 -0500

    correcting + values in longitude data

--- exam_2.R
+++ exam_2.R
@@ -1,1 +1,8 @@
-#testing github connection 
+#testing github connection
+#fixing longitude values 
+a=read.csv("Alaska_Bald_Eagle_Nest_Sites_(USFWS).csv")
+colnames(a)
+a[,7]<-abs(a[,7])
+a[,7]<-a[,7]*-1
+View(a)
+
