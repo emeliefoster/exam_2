2de8b80e49111bb8ee5c4bd9f5f9a8ab775c9542
emeliefoster <emf6966@louisiana.edu>
2021-04-01 14:05:58 -0500

    adding a date column using a mm/dd/yyyy format

--- exam_2.R
+++ exam_2.R
@@ -5,4 +5,9 @@ colnames(a)
 a[,7]<-abs(a[,7])
 a[,7]<-a[,7]*-1
 View(a)
-
+#adding a date column using a mm/dd/yyyy format 
+library(dplyr)
+b <- a%>%
+  mutate(date = as.character(as.Date(modayyr, "%Y/%m/%d"),"%m/%d/%Y"))
+View(b)
+write.csv(x=b, file="Nest_Sites_corrected.csv", row.names = F)
